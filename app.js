9231c49ab6557ef1e15a73f61c6b852f14af21d2
Aaron Schechter <aaron.m.schechter@gmail.com>
2022-09-29 20:59:48 -0700

    committing vue files, working vue render

--- app.js
+++ app.js
@@ -4,16 +4,12 @@ const path = require('path');
 
 const port = 3000;
 
-// View engine setup
-//app.set('view engine', 'html');
-
 app.get('/', (req,res) => {
     //res.render('index');
     res.sendFile(path.join(__dirname+'/index.html'));
+ 
 
 });
-
-
 
 app.listen(port, () => {
     console.log('listening!');

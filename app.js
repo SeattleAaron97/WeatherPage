5da530dbe815907521cb9f00f54881eb11f8032f
Aaron Schechter <aaron.m.schechter@gmail.com>
2022-10-08 09:16:12 -0700

    added axios, made some small updates and api calls

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+const axios = require('axios');
+
 const express = require('express');
 const app = express();
 const path = require('path');
+
 
 const port = 3000;
 
@@ -10,6 +13,24 @@ app.get('/', (req,res) => {
  
 
 });
+
+//https://api.weather.gov/points/30.2848,97.7176 //haymaker??
+
+//https://api.weather.gov/gridpoints/TOP/31,80/forecast //???
+
+//https://api.weather.gov/points/39.7456,-97.0892 //example req
+
+
+//https://api.weather.gov/alerts/active?area=WA
+
+console.log('above the get');
+axios.get('https://api.weather.gov/alerts/active?area=WA').then(function (response) {
+    console.log('successfully got?');
+    console.log('more?');
+    console.log(response.data.features[0].properties);
+}).catch(function (error) {
+    console.log('error - ', error);
+})
 
 app.listen(port, () => {
     console.log('listening!');
